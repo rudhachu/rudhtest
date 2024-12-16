c0113f707d47d3933a1537f012ea8eaa56a21491
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-12-16 12:20:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
   BRANCH: "main",
   ADMIN_ACCESS: toBool(process.env.ADMIN_ACCESS) || false,
+  ANTI_DELETE: toBool(process.env.ANTI_DELETE) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   ANTILINK: toBool(process.env.ANTI_LINK) || true,
   AUTHOR: process.env.AUTHOR || "Ƥ ʀ ɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ",
